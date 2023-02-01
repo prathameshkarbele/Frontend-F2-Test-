64dac59019d77ac6398268f2f2e07543e174968e
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-02-01 19:46:49 +0530

    chnges

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// const img1 = document.getElementById("img1");
+// const formContainer = document.getElementById("formContainer");
+// const registerBtn = document.getElementById("registerBtn");
+// const displayContainer = document.getElementById("displayContainer");
+// let img1Clicked = false;
+
+// img1.addEventListener("click", function() {
+//   if (!img1Clicked) {
+//     formContainer.style.display = "block";
+//     img1Clicked = true;
+//   }
+// });
+
+// registerBtn.addEventListener("click", function() {
+//   const name = document.getElementById("name").value;
+//   const username = document.getElementById("username").value;
+//   localStorage.setItem("name", name);
+//   localStorage.setItem("username", username);
+//   formContainer.style.display = "none";
+// });
+
+// const img2 = document.getElementById("img2");
+// img2.addEventListener("click", function() {
+//   displayContainer.style.display = "block";
+//   formContainer.style.display = "none";
+//   const name = localStorage.getItem("name");
+//   const username = localStorage.getItem("username");
+//   document.getElementById("displayName").innerHTML = "Name: " + name;
+//   document.getElementById("displayUsername").innerHTML = "Username: " + username;
+// });
